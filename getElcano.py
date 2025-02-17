cc78b1694ebc6180fb0eea4f1e7f019e5d40dae4
M1NEIPTV <132680797+M1NEIPTV@users.noreply.github.com>
2025-02-17 23:18:11 +0100

    Actualizar getElcano.py

--- getElcano.py
+++ getElcano.py
@@ -3,8 +3,8 @@ import requests
 import os
 
 # Define the URL and output file path
-url = "https://proxy.zeronet.dev/18D6dPcsjLrjg2hhnYqKzNh2W6QtXrDwF/links.json"
-#url = "https://bafybeihaak6jp7c6y3weyynkfruefwtgchvx2uuppmtts775k4z7kyw4ny.ipfs.dweb.link/?filename=index.html/links.json"
+#url = "https://proxy.zeronet.dev/18D6dPcsjLrjg2hhnYqKzNh2W6QtXrDwF/links.json"
+url = "https://proxy.zeronet.dev/1JKe3VPvFe35bm1aiHdD4p1xcGCkZKhH3Q"
 output_file = "toys/cachedList.txt"
 
 
